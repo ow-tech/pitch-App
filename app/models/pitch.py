59312ba9b836334c5b125d09c0103984ea297ae9
ow-tech <wagwanwekon@outloo.com>
2020-05-02 13:52:47 +0300

    sucessfully created a test for the pitch class

---
+++ app/models/pitch.py
@@ -0,0 +1,7 @@
+class Pitch:
+    """
+    Pitch class to define Pitch objects
+    """
+
+    def __init__ (self, your_pitch):
+        self.your_pitch = your_pitch
--- app/pitch_test_.py
+++ app/pitch_test_.py
@@ -1,9 +1,23 @@
 import unittest
 from models import pitch
 
+Pitch = pitch.Pitch
+
 class PitchTest(unittest.TestCase):
     '''
     Test Class to test the behaviour of the pitch class
 
     '''
-    
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_pitch = Pitch("I recently graduated from college with a degree in communications. I worked on the college newspaper as a reporter, and eventually, as the editor of the arts section. I'm looking for a job that will put my skills as a journalist to work")
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_pitch,Pitch))
+
+
+if __name__ == '__main__':
+    unittest.main()
