87f5043ff689838cfefccd8ac1762ed5303b5986
ow-tech <wagwanwekon@outloo.com>
2020-05-03 13:25:50 +0300

    creation of registration form in forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-# from wtforms.validators import Required
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField
+from wtforms.validators import Required, Length, Email, EqualTo
 
 
 class Your_pitchForm(FlaskForm):
@@ -8,3 +8,11 @@ class Your_pitchForm(FlaskForm):
     category = StringField("Category")
     Your_pitch = TextAreaField('Your Pitch')
     post = SubmitField('Submit')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username' validators=[Required(),Length(min=2, max=50)])
+    email = StringField('Email', validators=[Required(),Email()])
+    password = PasswordField('Password', validators=[Required()]) 
+    confirm_password = PasswordField('Confirm Password', validators=[Required(), EqualTo('password')])
+
+    submit = SubmitField('sign Up')     
